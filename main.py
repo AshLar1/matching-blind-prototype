e129cc9f705a34b5f48d65a7d3846c17c39a744d
Ashley  Lartey <38960526-ashleylartey@users.noreply.replit.com>
2025-04-28 09:48:47 +0000

    First commit - Landing page and app prototype setup

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def index():
-    return 'Hello from Flask!'
+def landing():
+    return render_template('landing.html')
 
-if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=5000)
+
+@app.route('/app')
+def app_page():
+    return render_template('app.html')
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=81, debug=True)
